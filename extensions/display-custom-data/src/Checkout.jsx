6262f3930c32cff82695de88f749189315d0fac2
saturn-ss <shopify.saturn@outlook.com>
2025-05-02 08:04:12 -0400

    Refactor Checkout component to improve metafield handling and update function naming

--- extensions/display-custom-data/src/Checkout.jsx
+++ extensions/display-custom-data/src/Checkout.jsx
@@ -6,33 +6,30 @@ import {
   reactExtension,
 } from "@shopify/ui-extensions-react/checkout";
 
-// 1. Choose an extension target
-export default reactExtension("purchase.checkout.cart-line-item.render-after", () => (
-  <Extension />
-));
+// Set the entry points for the extension
+export default reactExtension("purchase.checkout.cart-line-item.render-after", () => <App />);
 
-function Extension() {
+function App() {
   // Use the merchant-defined metafield for watering instructions and map it to a cart line
-  const wateringmetafields = useAppMetafields({
+  const wateringMetafields = useAppMetafields({
     type: "product",
     namespace: "instructions",
     key: "watering"
   });
-
   const cartLineTarget = useCartLineTarget();
 
-  const [wateringInstructions, setWateringInstructions] = useState("llll");
+  const [wateringInstructions, setWateringInstructions] = useState("");
 
   useEffect(() => {
     // Get the product ID from the cart line item
-    const productId = cartLineTarget?.merchandies?.product?.id;
+    const productId = cartLineTarget?.merchandise?.product?.id;
     if (!productId) {
       return;
     }
 
-    const wateringMetafield = wateringmetafields.find(({ target }) => {
-      // Check if the target of the metafields is the product from our cart line
-      reutrn `gid://shopify/Product/${target.id}` === productId;
+    const wateringMetafield = wateringMetafields.find(({target}) => {
+      // Check if the target of the metafield is the product from our cart line
+      return `gid://shopify/Product/${target.id}` === productId;
     });
 
     // If we find the metafield, set the watering instructions for this cart line
@@ -44,11 +41,11 @@ function Extension() {
   // Render the watering instructions if applicable
   if (wateringInstructions) {
     return (
-      <Text>
-        {wateringInstructions}
-      </Text>
-    );
+        <Text>
+          {wateringInstructions}
+        </Text>
+      );
   }
 
-  return <Text>Good</Text>;
+  return null;
 }
